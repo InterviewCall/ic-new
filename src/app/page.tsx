00e07853a66155f06f7ed2d42014b486499ebfd3
Pratham <sinhapratham1234@gmail.com>
2025-12-17 14:29:01 +0530

    #feat: till student success stories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,20 @@ import HeroMarque from "./components/HeroMarque";
 import StatsTrustSection from "./components/StatsTrustSection";
 import Roadmap from "./components/Roadmap";
 import MeetYourInstructors from "./components/MeetYourInstructors";
+import StudentSuccessStories from "./components/StudentSuccessStories";
 
 export default function Home() {
   return (
     <div className="w-full h-full">
       <InterviewCallHero />
       <HeroMarque />
-      <div className="w-full flex flex-col items-center">
+      <div className="w-full flex flex-col items-center ">
         <div className="w-85/100">
           <StatsTrustSection />
           <JourneyComparison />
           <Roadmap />
           <MeetYourInstructors />
+          <StudentSuccessStories />
         </div>
       </div>
     </div>
