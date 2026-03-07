87d442bdfce0f5393f6e229e2fabebdc1fc40192
Arijit Ganguly <aganguly158@gmail.com>
2026-03-08 03:32:32 +0530

    #fix: fixed the title, description and favicon of the site

---
+++ src/constants/config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  title: 'InterviewCall',
+  description:
+    'InterviewCall is your go to go platform for interview preparation. Find expert tips, mock interviews and more!',
+  url: 'https://www.interviewcall.club',
+};
