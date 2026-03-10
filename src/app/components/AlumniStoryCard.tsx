c73b53cae77eb1784f93ed9a7a3404fb990c5b15
Pratham <sinhapratham1234@gmail.com>
2026-03-10 20:12:44 +0530

    #fix: created custom dropdown for forms and changes function of india map

--- src/app/components/AlumniStoryCard.tsx
+++ src/app/components/AlumniStoryCard.tsx
@@ -4,21 +4,24 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function AlumniStoryCard() {
+    const [activeDotId, setActiveDotId] = useState<string | null>("8");
     return (
-        <div className="relative max-md:hidden flex items-center justify-center rounded-4xl h-125">
+        <div
+            onMouseLeave={()=>{setActiveDotId("8")}}
+            className="relative max-md:hidden flex items-center justify-center rounded-4xl h-125">
                 <Image src={'/indiaMap.svg'} alt="" height={1000} width={1000} className="absolute rounded-4xl left-0 h-full w-full object-cover" />
-                <Dot left="254" top="85" alumniSrc={'/cohortPfp1.svg'} name={'Rohit'} />
-                <Dot left="222" top="103" alumniSrc={'/cohortPfp1.svg'} name={'Shreya'} />
-                <Dot left="209" top="127" alumniSrc={'/cohortPfp1.svg'} name={'Rohit'} />
-                <Dot left="194" top="174" alumniSrc={'/cohortPfp1.svg'} name={'Shreya'} />
-                <Dot left="435" top="178" alumniSrc={'/cohortPfp1.svg'} name={'Rohit'} />
-                <Dot left="355" top="244" alumniSrc={'/cohortPfp1.svg'} name={'Shreya'} />
-                <Dot left="262" top="197" alumniSrc={'/cohortPfp1.svg'} name={'Rohit'} />
-                <Dot left="200" top="250" alumniSrc={'/cohortPfp1.svg'} name={'Shreya'} />
-                <Dot left="250" top="257" alumniSrc={'/cohortPfp1.svg'} name={'Rohit'} />
-                <Dot left="219" top="400" alumniSrc={'/cohortPfp1.svg'} name={'Shreya'} />
-                <Dot left="239" top="350" alumniSrc={'/cohortPfp1.svg'} name={'Shreya'} />
-                <Dot left="209" top="330" alumniSrc={'/cohortPfp1.svg'} name={'Shreya'} />
+                <Dot id="1" left="254" top="85" alumniSrc={'/cohortPfp1.svg'} name={'Rohit'} isActive={activeDotId === '1'} onHover={() => setActiveDotId('1')} onLeave={() => setActiveDotId(null)}/>
+                <Dot id="2" left="222" top="103" alumniSrc={'/cohortPfp1.svg'} name={'Shreya'} isActive={activeDotId === '2'} onHover={() => setActiveDotId('2')} onLeave={() => setActiveDotId(null)}/>
+                <Dot id="3" left="209" top="127" alumniSrc={'/cohortPfp1.svg'} name={'Rohit'} isActive={activeDotId === '3'} onHover={() => setActiveDotId('3')} onLeave={() => setActiveDotId(null)}/>
+                <Dot id="4" left="194" top="174" alumniSrc={'/cohortPfp1.svg'} name={'Shreya'} isActive={activeDotId === '4'} onHover={() => setActiveDotId('4')} onLeave={() => setActiveDotId(null)}/>
+                <Dot id="5" left="435" top="178" alumniSrc={'/cohortPfp1.svg'} name={'Rohit'} isActive={activeDotId === '5'} onHover={() => setActiveDotId('5')} onLeave={() => setActiveDotId(null)}/>
+                <Dot id="6" left="355" top="244" alumniSrc={'/cohortPfp1.svg'} name={'Shreya'} isActive={activeDotId === '6'} onHover={() => setActiveDotId('6')} onLeave={() => setActiveDotId(null)}/>
+                <Dot id="7" left="242" top="197" alumniSrc={'/cohortPfp1.svg'} name={'Rohit'} isActive={activeDotId === '7'} onHover={() => setActiveDotId('7')} onLeave={() => setActiveDotId(null)}/>
+                <Dot id="8" left="200" top="250" alumniSrc={'/cohortPfp1.svg'} name={'Shreya'} isActive={activeDotId === '8'} onHover={() => setActiveDotId('8')} onLeave={() => setActiveDotId(null)}/>
+                <Dot id="9" left="280" top="257" alumniSrc={'/cohortPfp1.svg'} name={'Rohit'} isActive={activeDotId === '9'} onHover={() => setActiveDotId('9')} onLeave={() => setActiveDotId(null)}/>
+                <Dot id="10" left="219" top="400" alumniSrc={'/cohortPfp1.svg'} name={'Shreya'} isActive={activeDotId === '10'} onHover={() => setActiveDotId('10')} onLeave={() => setActiveDotId(null)}/>
+                <Dot id="11" left="239" top="350" alumniSrc={'/cohortPfp1.svg'} name={'Shreya'} isActive={activeDotId === '11'} onHover={() => setActiveDotId('11')} onLeave={() => setActiveDotId(null)}/>
+                <Dot id="12" left="209" top="330" alumniSrc={'/cohortPfp1.svg'} name={'Shreya'} isActive={activeDotId === '12'} onHover={() => setActiveDotId('12')} onLeave={() => setActiveDotId(null)}/>
 
 
                 {/* <AlumniCard /> */}
@@ -45,28 +48,19 @@ function AlumniCard({ currentAlumni }: { currentAlumni: { src: string, name: str
     )
 }
 
-function Dot({ top, left, alumniSrc, name }: { top: string, left: string, alumniSrc: string, name: string }) {
-    const [currentAlumni, setCurrentAlumni] = useState<{ src: string, name: string } | null>(null);
-
-    function handleMouseEnter() {
-        setCurrentAlumni({ src: alumniSrc, name });
-    }
-
-    function handleMouseLeave() {
-        setCurrentAlumni(null);
-    }
+function Dot({ top, left, alumniSrc, name, isActive, onHover, onLeave }: { id: string, top: string, left: string, alumniSrc: string, name: string, isActive: boolean, onHover: () => void, onLeave: () => void}) {
     return (
         <div
             style={{ top: `${top}px`, left: `${left}px` }}
             className="absolute"
         >
             <div
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
+                onMouseEnter={onHover}
+                onMouseLeave={onLeave}
                 className="relative cursor-pointer rounded-full h-2 w-2 bg-linear-to-tr from-[#67B1F5] to-[#245AB5] shadow-[0_0_10px_1px_rgba(103,177,245,0.9)]"
             >
             
-                <AlumniCard currentAlumni={currentAlumni} />
+                <AlumniCard currentAlumni={isActive ? { src: alumniSrc, name } : null} />
                 
 
             </div>
